f1323c2c3fea3f8ffcdd5511c5f3c51f6c0dcadd
Jenny Shih <jinghua.shih@gmai.com>
2019-02-11 21:24:01 +0800

    minor fix

--- lib/medium-scraper.rb
+++ lib/medium-scraper.rb
@@ -2,23 +2,24 @@ require 'scraper/version'
 require 'scraper/user-loader'
 require 'scraper/post-scraper'
 
-class MediumScraper
-  def posts_by_user(account, **opts)
-    posts = UserLoader.new(account).all_posts
+module MediumScraper
+  class << self
+    def posts_by_user(account, **opts)
+      posts = UserLoader.new(account).all_posts
 
-    if opts['order'] == 'published_asc'
-      posts = posts.reverse
+      if opts['order'] == 'published_asc'
+        posts = posts.reverse
+      end
+      if opts['count']
+        posts = posts.first(opts['count'].to_i)
+      end
+
+      posts
     end
 
-    if opts['count']
-      posts = posts.first(opts['count'].to_i)
+    def post_by_url(url)
+      PostScraper.parse(url)
     end
-
-    posts
-  end
-
-  def post_by_url(url)
-    PostScraper.parse(url)
   end
 end
 
--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -1,4 +1,3 @@
-# require 'medium-scraper'
 require 'thor'
 module MediumScraper
   class CLI < Thor
