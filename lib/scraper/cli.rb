ff27a1745c33b9e86fbefd149f76bc8625c78dfb
Jenny Shih <jinghua.shih@gmai.com>
2019-02-12 06:26:07 +0800

    minor fix

--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -1,14 +1,16 @@
 require 'thor'
+require 'medium-scraper'
+
 module MediumScraper
   class CLI < Thor
     desc "user USER", "Returns a list of posts by this user account"
     def user(user)
-      MediumScraper.posts_by_user(user)
+      puts MediumScraper::posts_by_user(user)
     end
 
     desc "url URL", "Returns content for a Medium post by its url"
     def url(url)
-      MediumScraper.post_by_url(url)
+      puts MediumScraper::post_by_url(url)
     end
   end
 end
