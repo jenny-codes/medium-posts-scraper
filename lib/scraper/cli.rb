8789d72411a20db55e2d3f7f3693c889d751d172
Jenny Shih <jinghua.shih@gmai.com>
2019-02-11 17:50:37 +0800

    added client

---
+++ lib/scraper/cli.rb
@@ -0,0 +1,15 @@
+# require 'medium-scraper'
+require 'thor'
+module MediumScraper
+  class CLI < Thor
+    desc "user USER", "Returns a list of posts by this user account"
+    def user(user)
+      MediumScraper.posts_by_user(user)
+    end
+
+    desc "url URL", "Returns content for a Medium post by its url"
+    def url(url)
+      MediumScraper.post_by_url(url)
+    end
+  end
+end
